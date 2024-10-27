a6d25d36bd7fcadbefc48749a140a935476c1f3f
karol-kkk <karol.m99@interia.pl>
2024-10-27 15:53:16 +0100

    update

--- myprogram.py
+++ myprogram.py
@@ -76,3 +76,15 @@ print("Northern Hemisphere in %: ", north/total*100)
 print("Southern Hemisphere: ", south)
 print("Southern Hemisphere in %: ", south/total*100)
 
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = "23"
+age = int(age)
+height = "150"
+height = int(height)
+agesix = age + 6
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm")
+print(f"In 6 years I will be {agesix} years old.")
