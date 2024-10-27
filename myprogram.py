64548d433af26cd73210b5c480e50c1a5f324a21
karol-kkk <karol.m99@interia.pl>
2024-10-27 16:00:37 +0100

    update

--- myprogram.py
+++ myprogram.py
@@ -88,3 +88,22 @@ agesix = age + 6
 print(f"My name is {name}.")
 print(f"I am {age} years old, and my height is {height} cm")
 print(f"In 6 years I will be {agesix} years old.")
+
+###
+# Write a program that calculates and prints
+# the income of a family per person. To print the results
+# in a readable form, use f-strings.
+#
+father_income = 5450
+mother_income = 4920
+family_members = 5
+total_income = father_income + mother_income
+income_per_person = total_income / family_members
+print(f'Total family income is {total_income}, and income per person is {income_per_person}')
+
+a = 3
+b = 5
+print(f'{a}+{b}= {a +b}')
+print(f'{a}-{b}= {a - b}')
+print(f'{a}*{b}= {a * b}')
+print(f'{a}/{b}= {a/b}')
